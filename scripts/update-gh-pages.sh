f35e6f3c8405a59d8ca5d8268daca1590b90231b
odgit <o.dmitricenko@gmail.com>
2021-01-02 18:54:05 +0100

    remote deploy of gh-pages

---
+++ scripts/update-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+repo_uri="https://x-access-token:${DEPLOY_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_name="origin"
+main_branch="master"
+target_branch="gh-pages"
+build_dir="dist"
+
+cd "$GITHUB_WORKSPACE"
+
+git config user.name "$GITHUB_ACTOR"
+git config user.email "{$GITHUB_ACTOR}@bots.github.com"
