555407d6ae6d0a1699e82e3295078cedc0edba53
odgit <o.dmitricenko@gmail.com>
2021-02-10 00:00:08 +0100

    lint fix

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,18 @@
-//TODO:
+// TODO:
 // FROM REMOTE API
 // add publisher, year published,
 // author, rating, rarity
 // in COLLECTION
 // State: own, would like to have, collectable?
 
-
+// ts-ignore no-unused-vars
 namespace Collection {
   export interface Game {
-    title: string
-    platform: string
-    bootleg: boolean
-    region: string
+    title: string;
+    platform: string;
+    bootleg: boolean;
+    region: string;
+    series?: string;
+    hasBattery?: boolean;
   }
 }
